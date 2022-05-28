2f3e4182496fbb34391d81dc05094240365b05aa
MosesShobo <104919675+MosesShobo@users.noreply.github.com>
2022-05-28 20:44:44 +0500

    update message as per instructions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     const getId = async () => {
       try {
         const resp = await fetch(API_URL)
-        setSuccessMessage("Frotend connected Successfully to Backend, Please check https://github.com/MosesShobo/devops-code-challenge for further detail and setup server")
+        setSuccessMessage("SUCCESS")
       }
       catch(e) {
         setFailureMessage(e.message)
